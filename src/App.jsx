53a29aab19a000c687dc0dce57eca244e3d2225d
matth9597 <matthraf97@gmail.com>
2026-04-25 01:07:44 -0400

    adding home button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Details from './pages/Details';
 import './App.css';
@@ -7,10 +7,10 @@ import { sportsData } from './data';
 export default function App() {
   const location = useLocation();
   const sportId = location.state?.id;
-
   const slugFromUrl = location.pathname.split('/')[1]; 
   const currentSport = Object.values(sportsData).find(s => s.slug === slugFromUrl);
   const texteBande = location.pathname === '/' ? 'Disciplines' : (currentSport?.categorie || 'DÉTAILS');
+  const navigate = useNavigate();
 
   return (
     <div className="site-wrapper">
@@ -22,7 +22,18 @@ export default function App() {
       </header>
 
       <div className="sub-header-bande">
-        {texteBande}
+        <div className="bande-content">
+          {location.pathname !== '/' && (
+            <>
+              <button className="btn-home-simple" onClick={() => navigate('/')}>
+                <img src="/home.png" alt="Home" className="home-icon-img" />
+              </button>
+              {/* VOICI LE VRAI TRAIT SÉPARATEUR */}
+              <span className="separateur-vertical"></span>
+            </>
+          )}
+          <span className="bande-title-left">{texteBande}</span>
+        </div>
       </div>
 
       <main className="main-content">
