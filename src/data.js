dbf6f03e10ca48c3de2c797deb1d820215508db3
Matthieu Ramanitrera <mrama100@uottawa.ca>
2026-05-05 21:00:02 -0400

    feat: add proper registration links
    TODO: Sora-tononina and unconfirmed categories

--- src/data.js
+++ src/data.js
@@ -2,15 +2,21 @@ export const sportsData = {
   soccer_h: {
     categorie: "soccerHommes",
     slug: "soccer-homme",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "11 vs 11",
         texteEN: "11 vs 11",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -33,23 +39,27 @@ export const sportsData = {
         texteEN: "Natural (elimination rounds) and synthetic (final)",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   soccer_v: {
     categorie: "soccerVeterans",
     slug: "soccer-veteran",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLSf14r9P71mofDVNvdTPwZxS9vh9Xbh47x31dQ7NPXIFs85ljQ/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLSegUMeyduQNCUme-qPXVqVBpUN7E4_W08YTptniHGC_epNQ7w/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "9vs9, sans hors-jeux",
         texteEN: "9v9, no offsides",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -72,23 +82,27 @@ export const sportsData = {
         texteEN: "Natural grass (Half) and synthetic grass (Half)",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
-  
+
   soccer_m: {
     categorie: "soccerMixte",
     slug: "soccer-mixte",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLSdNcD3uD10LzAHyvprNNc9iKJmU8bEOIPqDm4xplEfmRorQNA/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLSfZvBfDWLHn55xqUG81SwA8uv6obp46QAUr2oa2BYEZT06XMA/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "9vs9, sans hors-jeux",
         texteEN: "9v9, no offsides",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -111,23 +125,27 @@ export const sportsData = {
         texteEN: "Natural grass (Half) and synthetic grass (Half)",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   soccer_k: {
     categorie: "soccerKids",
     slug: "soccer-kids",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLSeUIFKKAZ1xp9l5rGGVnhIW72zRy7CDeCkETbZtsfszP1Q-tw/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLSem700_y7hbSIy5_75EA0UIR-4HL7kRkXZaQrXTS3L7VSxvgA/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "7vs7, sans hors-jeux",
         texteEN: "7v7, no offsides",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -150,23 +168,27 @@ export const sportsData = {
         texteEN: "Natural and synthetic grass",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   basketball_f: {
     categorie: "basketballFemmes",
     slug: "basketball-femmes",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLSenaEuzHUqq4DzQ73a82B5Ydznvb1JRumrekRg50OZzz_t51Q/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLScBLtfoIKhdcM1nmpgGb0_sey8UmwMl4SbTvvSul-xOxWZe8Q/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "5 vs 5",
         texteEN: "5 vs 5",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -189,23 +211,27 @@ export const sportsData = {
         texteEN: "Indoor gymnasium",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   basketball_h: {
     categorie: "basketballHommes",
     slug: "basketball-homme",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLSfy7Foco9iKZ2BSpc4uVy2opINXoKmt6mg_F9pIIiL7H3t10A/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLScIIMH-9Nl4FIKhUD0GP1aePtAzmW9BVpQnwXc_wlpEF_gauQ/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "5 vs 5",
         texteEN: "5 vs 5",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -228,23 +254,27 @@ export const sportsData = {
         texteEN: "Indoor gymnasium",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   basketball_j: {
     categorie: "basketballJunior",
     slug: "basketball-junior",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLSeK3VM2pu8im5T6h27frM1LsZcKb3q_sR_gWZZKh7cuF_HwAQ/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLScszvrGwHd1F9Xejg10xQAdbpJFJ8iFY4GD2EpgUoNm2QXW9A/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "5 vs 5",
         texteEN: "5 vs 5",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -267,23 +297,27 @@ export const sportsData = {
         texteEN: "Indoor gymnasium",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   basketball_k: {
     categorie: "basketballKids",
     slug: "basketball-kids",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLScXqhmftSfUorMseDHK4fnOT459mhaR-HD1Eic-iILN2HbNcw/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLSfcHclKRJ492--cG1Ksq15kSy--yqSMJv-r_mYHMEw2Mdr_og/viewform",
     sections: [
       { nom: "type", texteFR: "Mini-tournoi", texteEN: "Mini-tournament" },
       {
         nom: "format",
         texteFR: "5 vs 5",
         texteEN: "5 vs 5",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -306,23 +340,27 @@ export const sportsData = {
         texteEN: "Indoor gymnasium",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   volleyball_f: {
     categorie: "volleyballFemmes",
     slug: "volleyball-femme",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLScN65egh-Z7wn0hpw_PjhbfVj-mSYUebkFYLAHKoom2x1k29Q/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLSdK2wJDeStCQEplolmKjUPX3ZOaLyW0CDhg9ujAg64sJygv8A/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "6 vs 6",
         texteEN: "6 vs 6",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -345,23 +383,27 @@ export const sportsData = {
         texteEN: "Indoor gymnasium",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   volleyball_h: {
     categorie: "volleyballHommes",
     slug: "volleyball-homme",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLSfT09A96ShXcNBVKH8FldchESx-f1aRDHsxFh5Vdpe4FEIaEw/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLSfUfkkQ_a6UKuyAtXhSVO5_eDv4udVkbh6GkeXFrKb_6-6JhQ/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "6 vs 6",
         texteEN: "6 vs 6",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -384,23 +426,25 @@ export const sportsData = {
         texteEN: "Indoor gymnasium",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   tennis: {
     categorie: "tennis",
     slug: "tennis",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLScI0UHtJYlz7xSU-yzKiDCj44efVJmcpNoAWImfj4LZWVVTCA/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "1 vs 1",
         texteEN: "1 vs 1",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -423,23 +467,27 @@ export const sportsData = {
         texteEN: "Acrylic surface",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   petanque: {
     categorie: "petanque",
     slug: "petanque",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
+    lienInscriptionEquipe:
+      "https://docs.google.com/forms/d/e/1FAIpQLScaT60iZcmE-dgEPVB4gyyZmFyJXx9DyLCJyIh6-xQ1KJTXUg/viewform",
+    lienInscriptionIndividuelle:
+      "https://docs.google.com/forms/d/e/1FAIpQLScNNlzU5HCLH6nZRRcqiMVfyvjaa91uyfCV7jJMOH3iyB214Q/viewform",
     sections: [
       { nom: "type", texteFR: "Compétition", texteEN: "Competition" },
       {
         nom: "format",
         texteFR: "Triplette",
         texteEN: "Triplet",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "anneeNaissance",
@@ -453,8 +501,10 @@ export const sportsData = {
       },
       {
         nom: "conditions",
-        texteFR: "Maximum 4 joueurs dans l'équipe (Minimum 2 joueurs pendant le jeu mais avec penalite - voir reglements)",
-        texteEN: "Maximum 4 players in the team (Minimum 2 players during the game but with a penalty - see rules)",
+        texteFR:
+          "Maximum 4 joueurs dans l'équipe (Minimum 2 joueurs pendant le jeu mais avec penalite - voir reglements)",
+        texteEN:
+          "Maximum 4 players in the team (Minimum 2 players during the game but with a penalty - see rules)",
       },
       {
         nom: "terrain",
@@ -462,22 +512,22 @@ export const sportsData = {
         texteEN: "Fine gravel",
       },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 
   soraTononina: {
     categorie: "soraTononina",
     slug: "sora-tononina",
-    lienInscriptionEquipe:"https://docs.google.com/forms/d/e/1FAIpQLSeKssMyz6YTZtaXliDHH1vGWpy-VNpkxtXMZ9KYaqOIR9o8jQ/viewform?",
-    lienInscriptionIndividuelle:"https://docs.google.com/forms/d/e/1FAIpQLSdPU8TNFat0hiTM6pgyZiEYopx7pL5oyztgBKL7VBJ3kHXTfA/viewform?",
     sections: [
       {
         nom: "format",
         texteFR: "",
         texteEN: "",
-        lien: { url: "reglement", texteFR: "reglements", texteEN: "regulations" },
+        lien: {
+          url: "reglement",
+          texteFR: "reglements",
+          texteEN: "regulations",
+        },
       },
       {
         nom: "frais",
@@ -488,10 +538,8 @@ export const sportsData = {
         nom: "conditions",
         texteFR: "",
         texteEN: "",
-      }
+      },
     ],
-    equipes: [
-      { nom: "ASC Zato", lienForm: "https://google.com" },
-    ],
+    equipes: [{ nom: "ASC Zato", lienForm: "https://google.com" }],
   },
 };
