46e06ecadaf0b34edc1a76a589ca24d3a8d786b7
matth9597 <matthraf97@gmail.com>
2026-07-01 22:22:17 -0400

    modif global calendar

--- src/pages/Programmation.jsx
+++ src/pages/Programmation.jsx
@@ -16,7 +16,7 @@ export default function Programmation() {
   const [siteFiltre, setSiteFiltre] = useState('all');
   const [sportFiltre, setSportFiltre] = useState('all'); 
 
-  const lienCalendrierGlobal = 'https://drive.google.com/file/d/19IoGBZKy-yXgCPpeHtJN2hW3IEt90JQ9/view?usp=drive_link';
+  const lienCalendrierGlobal = 'https://drive.google.com/file/d/1Rn_0wVkP92Up9QLU8Qk9LI4Kqvybd4cN/view?usp=drive_link';
 
   return (
     <div style={{ padding: '16px', backgroundColor: COULEURS.grisFond, minHeight: '100vh', display: 'flex', justifyContent: 'center' }}>
